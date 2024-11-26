7bf9cb68e5d5b24e718ae6c480438d0cc49b20d8
Mohamad Zeina <mohamadzeina@gmail.com>
2024-11-26 14:42:27 +0000

    Add topics csv to database

--- copy-openalex-csv.sql
+++ copy-openalex-csv.sql
@@ -47,3 +47,4 @@
 \copy openalex.works_referenced_works (work_id, referenced_work_id) from program 'gunzip -c csv-files/works_referenced_works.csv.gz' csv header
 \copy openalex.works_related_works (work_id, related_work_id) from program 'gunzip -c csv-files/works_related_works.csv.gz' csv header
 
+\copy openalex.works_topics (work_id, topic_id, display_name, score, subfield, field, domain) from program 'gunzip -c csv-files/works_topics.csv.gz' csv header
